9b1996524c6756e6ca69f5e4609fff9698ba7441
unknown <ldmhieudev@gmail.com>
2023-04-25 00:32:05 +0700

    first commit

---
+++ src/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path')
+const morgan = require('morgan');
+const engine = require('express-handlebars').engine
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname,"public")))
+
+//HTTP logger
+app.use(morgan('combined'))
+
+// engine express-handleabars
+app.engine('hbs', engine({
+  extname: ".hbs"
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname,'recources/views'));
+
+app.get('/', function (req, res) {
+  res.render('home')
+})
+
+app.get('/home-page',(req,res)=>{
+  res.render('homePage')
+})
+
+app.listen(port,()=>{
+    console.log(port)
+});
