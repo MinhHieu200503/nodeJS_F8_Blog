c605f8851fcfef4a53bf1ce4466ce2b3da7f904b
HieuLai <ldmhieudev@gmail.com>
2023-04-25 13:23:08 +0700

    Test chang port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const path = require('path')
 const morgan = require('morgan');
 const engine = require('express-handlebars').engine
 const app = express();
-const port = 3000;
+const port = 4000;
 
 app.use(express.static(path.join(__dirname,"public")))
 
@@ -15,7 +15,7 @@ app.engine('hbs', engine({
   extname: ".hbs"
 }));
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname,'recources/views'));
+app.set('views', path.join(__dirname,'resources/views'));
 
 app.get('/', function (req, res) {
   res.render('home')
@@ -26,5 +26,5 @@ app.get('/home-page',(req,res)=>{
 })
 
 app.listen(port,()=>{
-    console.log(port)
+    console.log("http://localhost:"+`${port}`)
 });
